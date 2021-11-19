3b5a5de5a9b956bd4990b387b3e08983eec62550
Sean M <smidgley792@gmail.com>
2021-11-19 11:14:58 -0800

    React Router Parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Link } from 'react-router-dom';
 
-function RenderDirectoryItem({ campsite, onClick }) {
-	return (
-		<Card>
-			<CardImg width="100%" src={campsite.image} alt={campsite.name} />
-			<CardImgOverlay>
-				<CardTitle>{campsite.name}</CardTitle>
-			</CardImgOverlay>
-		</Card>
+function RenderDirectoryItem({campsite}) {
+    return (
+        <Card>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
 	);
 }
 
@@ -29,34 +32,3 @@ function Directory(props) {
 }
 
 export default Directory;
-
-// import React, { Component } from 'react';
-// import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-
-// class Directory extends Component {
-
-//     render() {
-//         const directory = this.props.campsites.map(campsite => {
-//             return (
-//                 <div key={campsite.id} className="col-md-5 m-1">
-//                     <Card onClick={() => this.props.onClick(campsite.id)}>
-//                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-//                         <CardImgOverlay>
-//                             <CardTitle>{campsite.name}</CardTitle>
-//                         </CardImgOverlay>
-//                     </Card>
-//                 </div>
-//             );
-//         });
-
-//         return (
-//             <div className="container">
-//                 <div className="row">
-//                     {directory}
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// export default Directory;
